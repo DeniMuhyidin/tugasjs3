1f605544cb9e61537012967bf4940492388bc465
DeniMuhyidin <denimuhyidin209@gmail.com>
2022-02-03 13:46:52 +0700

    java

---
+++ soal4.js
@@ -0,0 +1,16 @@
+const obj = {
+    nama: "Deni",
+    kelas: "RPL 1",
+    umur: 17,
+    nikah: false,
+    Hp: {
+      nama: "Samsung A52 pro",
+      spesifikasiHp: {
+          chipset: "Snapdragon 720G",
+          ram: "8 GB",
+          rom: "128 GB",
+          camera: "64mp",
+          }
+      }
+    }
+    console.log(obj.Hp);
